dcf3f0292e5fbc5ff44d469248e555e449915714
Sébastien Riccio <sriccio@openbusiness.com>
2023-06-03 17:35:47 +0200

    Update Pylint config

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../spfmerge'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -14,10 +13,10 @@ release = 'v1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,8 +1,6 @@
-
 Welcome to spfmerge's documentation!
 ====================================
-
-.. automodule:: spfmerge.main
+.. automodule:: spfmerge.cli
     :members:
 
 .. toctree::
